f4f44b982b6b6199a37fd32ae6712503d4a8847c
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-19 20:18:24 -0300

    feat: add featured post logic in the home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,48 @@
 import { Container } from '@/components/Container';
 import { Header } from '@/components/Header';
+import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function Home() {
   return (
     <Container>
       <Header />
+
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
+          <Image
+            className='w-full h-full object-cover object-center group-hover:scale-105 transition'
+            src='/images/bryen_0.png'
+            alt='Post image'
+            width={1200}
+            height={720}
+            priority
+          />
+        </Link>
+        <div className='flex flex-col gap-4 sm: justify-center'>
+          <time
+            className='text-slate-600 block text-sm/tight'
+            dateTime='2025-08-19'
+          >
+            19/08/2025 00:00
+          </time>
+
+          <PostHeading as='h1' url='#'>
+            Lorem ipsum dolor sit amet consectetur
+          </PostHeading>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
+            optio facere nobis rerum provident ea. Natus nam debitis commodi
+            provident omnis maiores nostrum rem sed, quos dolores consectetur
+            nisi dicta!
+          </p>
+        </div>
+      </section>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
