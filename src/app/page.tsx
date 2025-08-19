0d8bb2bbee65ed310fead3432f661ae0ffbe4d09
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-19 17:40:34 -0300

    feat: add mock layout in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1>Home</h1>;
+import { PostsList } from '@/components/PostsList';
+import { SpinLoader } from '@/components/SpinLoader';
+import { Suspense } from 'react';
+
+export default async function Home() {
+  return (
+    <div>
+      <header>
+        <h1 className='text-center font-bold text-6xl'>Header</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <h1 className='text-center font-bold text-6xl'>Footer</h1>
+      </footer>
+    </div>
+  );
 }
