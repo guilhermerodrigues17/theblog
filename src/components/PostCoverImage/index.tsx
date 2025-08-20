3f00e00c4719bab864267ec4a4658a6434dc8af4
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-20 14:52:59 -0300

    feat: add post cover img component

---
+++ src/components/PostCoverImage/index.tsx
@@ -0,0 +1,56 @@
+import clsx from 'clsx';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+type PostCoverImageProps = {
+  href: string;
+  src: string;
+  alt: string;
+  width: number;
+  height: number;
+  priority?: boolean;
+  linkClassName?: string;
+  imageClassName?: string;
+};
+
+export function PostCoverImage({
+  href,
+  src,
+  alt,
+  width,
+  height,
+  priority = false,
+  linkClassName = '',
+  imageClassName = '',
+}: PostCoverImageProps) {
+  return (
+    <Link
+      className={clsx(
+        'w-full',
+        'h-full',
+        'overflow-hidden',
+        'rounded-xl',
+        linkClassName,
+      )}
+      href={href}
+    >
+      <Image
+        className={clsx(
+          'w-full',
+          'h-full',
+          'object-cover',
+          'object-center',
+          'group-hover:scale-105',
+          'transition',
+          imageClassName,
+        )}
+        src={src}
+        alt={alt}
+        width={width}
+        height={height}
+        priority={priority}
+      />
+    </Link>
+  );
+}
