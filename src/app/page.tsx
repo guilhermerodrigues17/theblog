5852c844fde94f4e61a5462df9f924258dc0e3b9
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-20 19:16:46 -0300

    feat: add featured post component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Container } from '@/components/Container';
+import { FeaturedPost } from '@/components/FeaturedPost';
 import { Header } from '@/components/Header';
-import { PostCoverImage } from '@/components/PostCoverImage';
-import { PostHeading } from '@/components/PostHeading';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import { Suspense } from 'react';
@@ -11,36 +10,7 @@ export default async function Home() {
     <Container>
       <Header />
 
-      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
-        <PostCoverImage
-          alt='Post image'
-          href='#'
-          src='/images/bryen_9.png'
-          width={1200}
-          height={720}
-          priority={true}
-        />
-
-        <div className='flex flex-col gap-4 sm:justify-center'>
-          <time
-            className='text-slate-600 block text-sm/tight'
-            dateTime='2025-08-19'
-          >
-            19/08/2025 00:00
-          </time>
-
-          <PostHeading as='h1' url='#'>
-            Lorem ipsum dolor sit amet consectetur
-          </PostHeading>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae
-            optio facere nobis rerum provident ea. Natus nam debitis commodi
-            provident omnis maiores nostrum rem sed, quos dolores consectetur
-            nisi dicta!
-          </p>
-        </div>
-      </section>
+      <FeaturedPost />
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
---
+++ src/components/FeaturedPost/index.tsx
@@ -0,0 +1,27 @@
+import { PostCoverImage } from '../PostCoverImage';
+import { PostDetails } from '../PostDetails';
+
+export function FeaturedPost() {
+  return (
+    <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+      <PostCoverImage
+        alt='Post image'
+        href='#'
+        src='/images/bryen_9.png'
+        width={1200}
+        height={720}
+        priority={true}
+      />
+
+      <PostDetails
+        createdAt='2025-08-19T00:00:00.000Z'
+        slug='#'
+        title='Lorem ipsum dolor sit amet consectetur'
+        excerpt='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae optio
+          facere nobis rerum provident ea. Natus nam debitis commodi provident
+          omnis maiores nostrum rem sed, quos dolores consectetur nisi dicta!'
+        as='h1'
+      />
+    </section>
+  );
+}
