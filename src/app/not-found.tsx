feb0fbf359ef8bd8c33cfcb668c30fa9b97da869
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-25 15:49:11 -0300

    fix: changing the title of the not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,21 @@ import clsx from 'clsx';
 
 export default function NotFoundPage() {
   return (
-    <div
-      className={clsx(
-        'min-h-[320px] bg-slate-900 text-slate-100',
-        'p-8 rounded-xl',
-        'flex items-center justify-center',
-        'text-center',
-      )}
-    >
-      <div>
-        <h1 className='text-7xl/tight font-bold mb-4'>404</h1>
-        <p>A página que você está tentando acessar não existe!</p>
+    <>
+      <title>Página não encontrada</title>
+      <div
+        className={clsx(
+          'min-h-[320px] bg-slate-900 text-slate-100',
+          'p-8 rounded-xl',
+          'flex items-center justify-center',
+          'text-center',
+        )}
+      >
+        <div>
+          <h1 className='text-7xl/tight font-bold mb-4'>404</h1>
+          <p>A página que você está tentando acessar não existe!</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
