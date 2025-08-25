3444e58dd9ecfecb053e483d35eb1127c06a91d9
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-25 15:38:14 -0300

    feat: add findById and slug cached queries

--- src/lib/post/queries.ts
+++ src/lib/post/queries.ts
@@ -1,6 +1,18 @@
 import { postRepository } from '@/repositories/post';
+import { notFound } from 'next/navigation';
 import { cache } from 'react';
 
 export const findAllPublicPostsCached = cache(
   async () => await postRepository.findAllPublic(),
 );
+
+export const findPostsByIdCached = cache(
+  async (id: string) => await postRepository.findById(id),
+);
+
+export const findPostsBySlugCached = cache(async (slug: string) => {
+  const post = await postRepository.findBySlug(slug).catch(() => undefined);
+  if (!post) notFound();
+
+  return post;
+});
