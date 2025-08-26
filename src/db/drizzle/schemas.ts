a4ae873cd8cee833239868ed8c97bdb5923b9d92
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-26 16:59:01 -0300

    feat: implements connection with pg and posts table schema

---
+++ src/db/drizzle/schemas.ts
@@ -0,0 +1,18 @@
+import { InferSelectModel } from 'drizzle-orm';
+import { boolean, pgTable, text } from 'drizzle-orm/pg-core';
+
+export const postsTable = pgTable('posts', {
+  id: text('id').primaryKey(),
+  slug: text('slug').notNull().unique(),
+  title: text('title').notNull(),
+  author: text('author').notNull(),
+  excerpt: text('excerpt').notNull(),
+  content: text('content').notNull(),
+  coverImgUrl: text('cover_img_url').notNull(),
+  published: boolean('published').notNull().default(false),
+  createdAt: text('created_at').notNull(),
+  updatedAt: text('updated_at').notNull(),
+});
+
+export type PostsSelectModel = InferSelectModel<typeof postsTable>;
+export type PostsInsertModel = InferSelectModel<typeof postsTable>;
