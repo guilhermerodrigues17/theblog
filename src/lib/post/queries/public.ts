f3d22c26cf20dd2ec331dcc144100813f2bd35a9
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-27 18:33:00 -0300

    feat: add data cache to the queries

--- src/lib/post/queries/public.ts
+++ src/lib/post/queries/public.ts
@@ -1,16 +1,29 @@
 import { postRepository } from '@/repositories/post';
+import { unstable_cache } from 'next/cache';
 import { notFound } from 'next/navigation';
 import { cache } from 'react';
 
 export const findAllPublicPostsCached = cache(
-  async () => await postRepository.findAllPublic(),
+  unstable_cache(
+    async () => {
+      return postRepository.findAllPublic();
+    },
+    ['posts'],
+    { tags: ['posts'] },
+  ),
 );
 
-export const findPublicPostBySlugCached = cache(async (slug: string) => {
-  const post = await postRepository
-    .findBySlugPublic(slug)
-    .catch(() => undefined);
-  if (!post) notFound();
+export const findPublicPostBySlugCached = cache((slug: string) => {
+  return unstable_cache(
+    async (slug: string) => {
+      const post = await postRepository
+        .findBySlugPublic(slug)
+        .catch(() => undefined);
+      if (!post) notFound();
 
-  return post;
+      return post;
+    },
+    [`post-${slug}`],
+    { tags: [`post-${slug}`] },
+  )(slug);
 });
