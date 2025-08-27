4f2ba50a41fdcbb11401c59826e6e49bcbdc93d5
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-27 18:33:33 -0300

    feat: add findAll method to admin queries

--- src/lib/post/queries/admin.ts
+++ src/lib/post/queries/admin.ts
@@ -1,6 +1,10 @@
 import { postRepository } from '@/repositories/post';
 import { cache } from 'react';
 
-export const findPostsByIdCached = cache(
+export const findPostByIdAdmin = cache(
   async (id: string) => await postRepository.findById(id),
 );
+
+export const findAllPostsAdmin = cache(async () => {
+  return postRepository.findAll();
+});
