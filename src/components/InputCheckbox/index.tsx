bde0c84efad89b5b212d85178a2f11b6d7a6e802
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-08-29 16:34:49 -0300

    feat: add input checkbox component

---
+++ src/components/InputCheckbox/index.tsx
@@ -0,0 +1,35 @@
+import clsx from 'clsx';
+import { useId } from 'react';
+
+type InputCheckboxProps = {
+  labelText?: string;
+  type?: 'checkbox';
+} & React.ComponentProps<'input'>;
+
+export function InputCheckbox({
+  labelText = '',
+  type = 'checkbox',
+  ...props
+}: InputCheckboxProps) {
+  const id = useId();
+  return (
+    <div className='flex items-center gap-3'>
+      <input
+        {...props}
+        className={clsx(
+          'w-4 h-4',
+          'outline-none',
+          'focus:ring-2 focus:ring-blue-600',
+          props.className,
+        )}
+        id={id}
+        type={type}
+      />
+      {labelText && (
+        <label htmlFor={id} className='text-sm'>
+          {labelText}
+        </label>
+      )}
+    </div>
+  );
+}
