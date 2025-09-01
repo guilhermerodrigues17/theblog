34845abd0d28c63130db18c70da030d806957b7e
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-01 18:24:37 -0300

    feat(newPost): implements the form fields layout

--- src/components/admin/ManagePostForm/index.tsx
+++ src/components/admin/ManagePostForm/index.tsx
@@ -12,19 +12,67 @@ export function ManagePostForm() {
   return (
     <form action=''>
       <div className='flex flex-col gap-6'>
-        <DefaultInput labelText='Nome' placeholder='Digite...' />
-        <DefaultInput labelText='Sobrenome' placeholder='Digite...' />
+        <DefaultInput
+          type='text'
+          labelText='ID'
+          placeholder='ID gerado automaticamente'
+          name='id'
+          readOnly
+          defaultValue={''}
+        />
 
-        <ImageUploader />
+        <DefaultInput
+          type='text'
+          labelText='Slug'
+          placeholder='Slug gerado automaticamente'
+          name='slug'
+          readOnly
+          defaultValue={''}
+        />
+
+        <DefaultInput
+          type='text'
+          labelText='Autor'
+          placeholder='Escreva o nome do autor do post...'
+          name='author'
+          defaultValue={''}
+        />
+
+        <DefaultInput
+          type='text'
+          labelText='Título'
+          placeholder='Escreva um título para o seu post...'
+          name='title'
+          defaultValue={''}
+        />
+
+        <DefaultInput
+          type='text'
+          labelText='Resumo'
+          placeholder='Escreva um resumo para o seu post...'
+          name='excerpt'
+          defaultValue={''}
+        />
 
         <MarkdownEditor
           labelText='Conteúdo'
-          textAreaName='content'
           value={contentValue}
           setValue={setContentValue}
+          textAreaName='content'
+          disabled={false}
         />
 
-        <InputCheckbox labelText='Selecionar' />
+        <ImageUploader />
+
+        <DefaultInput
+          type='text'
+          labelText='URL da imagem de capa'
+          placeholder='Escreva a url da imagem'
+          name='coverImageUrl'
+          defaultValue={''}
+        />
+
+        <InputCheckbox type='checkbox' labelText='Publicar?' name='published' />
 
         <div className='mt-6'>
           <DefaultButton type='submit'>Enviar</DefaultButton>
