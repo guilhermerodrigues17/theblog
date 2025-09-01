df1d01195fe6bde66d37e934a42c4fb09d108fd9
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-01 18:45:10 -0300

    feat(editPost): add ManagePostForm to the edit post page

--- src/app/admin/post/[id]/page.tsx
+++ src/app/admin/post/[id]/page.tsx
@@ -1,4 +1,14 @@
+import { ManagePostForm } from '@/components/admin/ManagePostForm';
+import { makePostDto } from '@/dto/post/dto';
+import { postRepository } from '@/repositories/post';
+import { Metadata } from 'next';
+import { notFound } from 'next/navigation';
+
 export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+  title: 'Editar post',
+};
 
 type AdminPostIdPageProps = {
   params: Promise<{ id: string }>;
@@ -8,6 +18,16 @@ export default async function AdminPostIdPage({
   params,
 }: AdminPostIdPageProps) {
   const { id } = await params;
+  const post = await postRepository.findById(id).catch(() => undefined);
 
-  return <div className='py-16 text-6xl'>AdminPostIdPage {id}</div>;
+  if (!post) notFound();
+
+  const postDto = makePostDto(post);
+
+  return (
+    <div className='flex flex-col gap-4'>
+      <h1 className='text-xl font-bold'>Editar post</h1>
+      <ManagePostForm post={postDto} />
+    </div>
+  );
 }
