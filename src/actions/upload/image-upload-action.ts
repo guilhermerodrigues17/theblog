9125a0b8cd8195e20ba03076253c38dc1682e5b6
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-01 17:32:06 -0300

    feat: implements create /upload dir and save image, returning the url

--- src/actions/upload/image-upload-action.ts
+++ src/actions/upload/image-upload-action.ts
@@ -1,6 +1,12 @@
 'use server';
 
-import { IMAGE_UPLOAD_MAX_SIZE } from '@/lib/constants';
+import {
+  IMAGE_SERVER_URL,
+  IMAGE_UPLOAD_DIR,
+  IMAGE_UPLOAD_MAX_SIZE,
+} from '@/lib/constants';
+import { mkdir, writeFile } from 'fs/promises';
+import { extname, resolve } from 'path';
 
 type ImageUploadActionResult = {
   url: string;
@@ -29,5 +35,20 @@ export async function imageUploadAction(
     return makeResult({ error: 'Imagem inválida' });
   }
 
-  return makeResult({ url: 'url' });
+  const imgExtension = extname(file.name);
+  const uniqueImgName = `${Date.now()}${imgExtension}`;
+
+  const uploadDirPath = resolve(process.cwd(), 'public', IMAGE_UPLOAD_DIR);
+  await mkdir(uploadDirPath, { recursive: true });
+
+  const fileArrBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(fileArrBuffer);
+
+  const fileFullPath = `${uploadDirPath}/${uniqueImgName}`;
+
+  await writeFile(fileFullPath, buffer);
+
+  const url = `${IMAGE_SERVER_URL}/${uniqueImgName}`;
+
+  return makeResult({ url });
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,5 @@
 export const RESPONSE_DELAY_IN_MS = 0;
+
 export const IMAGE_UPLOAD_MAX_SIZE = 921600; // 900Kb * 1024
+export const IMAGE_UPLOAD_DIR = 'upload';
+export const IMAGE_SERVER_URL = 'http://localhost:3000/upload';
