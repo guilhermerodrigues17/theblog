8ebb6b31aa75d3f6dba0a3f4c065c898148ff5aa
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-02 17:27:06 -0300

    feat: add a partial PostDto model fabric

--- src/dto/post/dto.ts
+++ src/dto/post/dto.ts
@@ -15,3 +15,17 @@ export const makePostDto = (post: PostModel): PostDto => {
     published: post.published,
   };
 };
+
+export const makePartialPostDto = (post?: Partial<PostModel>): PostDto => {
+  return {
+    id: post?.id || '',
+    slug: post?.slug || '',
+    title: post?.title || '',
+    author: post?.author || '',
+    excerpt: post?.excerpt || '',
+    content: post?.content || '',
+    coverImageUrl: post?.coverImageUrl || '',
+    createdAt: post?.createdAt || '',
+    published: post?.published || false,
+  };
+};
