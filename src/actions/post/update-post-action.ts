89e966afe1794f5df3e75c3de507830b587da284
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-03 16:24:14 -0300

    feat(updatePost): implements update post action

---
+++ src/actions/post/update-post-action.ts
@@ -0,0 +1,77 @@
+'use server';
+
+import {
+  makePartialPostDto,
+  makePostDto,
+  PostDto,
+} from '@/dto/post/general-dto';
+import { PostUpdateSchema } from '@/lib/post/validations';
+import { postRepository } from '@/repositories/post';
+import { getZodMessageError } from '@/utils/get-zod-error-message';
+import { revalidateTag } from 'next/cache';
+
+type UpdatePostActionState = {
+  formState: PostDto;
+  errors: string[];
+  success?: true;
+};
+
+export async function updatePostAction(
+  prevState: UpdatePostActionState,
+  formData: FormData,
+): Promise<UpdatePostActionState> {
+  if (!(formData instanceof FormData)) {
+    return {
+      formState: prevState.formState,
+      errors: ['Dados inválidos'],
+    };
+  }
+
+  const id = formData.get('id');
+  if (!id || typeof id !== 'string') {
+    return {
+      formState: prevState.formState,
+      errors: ['ID inválido'],
+    };
+  }
+
+  const formDataToObj = Object.fromEntries(formData.entries());
+  const zodParsedObj = PostUpdateSchema.safeParse(formDataToObj);
+
+  if (!zodParsedObj.success) {
+    const errors = getZodMessageError(zodParsedObj.error);
+    return {
+      errors,
+      formState: makePartialPostDto(formDataToObj),
+    };
+  }
+
+  const validPostData = zodParsedObj.data;
+  const updateData = {
+    ...validPostData,
+  };
+
+  let updatedPost;
+  try {
+    updatedPost = await postRepository.updatePost(id, updateData);
+    revalidateTag('posts');
+    revalidateTag(`post-${updatedPost.slug}`);
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      return {
+        formState: makePartialPostDto(updateData),
+        errors: [e.message],
+      };
+    }
+    return {
+      formState: makePartialPostDto(updateData),
+      errors: ['Ocorreu um erro...'],
+    };
+  }
+
+  return {
+    formState: makePostDto(updatedPost),
+    errors: [],
+    success: true,
+  };
+}
