cf3e9d43eb813be9389d985aca7f6dee98cfa218
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-04 17:17:53 -0300

    feat(login): implements methods to save and delete session cookies

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,7 +1,8 @@
 'use server';
 
-import { verifyPassword } from '@/lib/login/manage-login';
+import { createLoginSession, verifyPassword } from '@/lib/login/manage-login';
 import { asyncDelay } from '@/utils/async-delay';
+import { redirect } from 'next/navigation';
 
 type LoginActionState = {
   username: string;
@@ -41,8 +42,6 @@ export async function loginAction(state: LoginActionState, formData: FormData) {
     };
   }
 
-  return {
-    username: '',
-    error: '',
-  };
+  await createLoginSession(username);
+  redirect('/admin/post');
 }
--- src/lib/login/manage-login.ts
+++ src/lib/login/manage-login.ts
@@ -1,4 +1,9 @@
 import bcrypt from 'bcryptjs';
+import { cookies } from 'next/headers';
+
+const loginExpSeconds = Number(process.env.LOGIN_EXPIRATION_SECONDS) || 86400;
+const loginExpString = process.env.LOGIN_EXPIRATION_STRING || '1d';
+const loginCookieName = process.env.LOGIN_COOKIE_NAME || 'loginSession';
 
 export async function generateHash(password: string) {
   const hash = await bcrypt.hash(password, 10);
@@ -10,4 +15,23 @@ export async function verifyPassword(password: string, base64Hash: string) {
   const hash = Buffer.from(base64Hash, 'base64').toString('utf-8');
   const isValid = await bcrypt.compare(password, hash);
   return isValid;
+}
+
+export async function createLoginSession(username: string) {
+  const expiresIn = new Date(Date.now() + loginExpSeconds * 1000);
+  const loginSession = username + 'session_placeholder';
+  const cookieStore = await cookies();
+
+  cookieStore.set(loginCookieName, loginSession, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+    expires: expiresIn,
+  });
+}
+
+export async function deleteLoginSession() {
+  const cookieStore = await cookies();
+  cookieStore.set(loginCookieName, '', { expires: new Date(0) });
+  cookieStore.delete(loginCookieName);
 }
