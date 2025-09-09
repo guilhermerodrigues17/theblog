ac3e52a638aeebf8ab018c9e0a02a21a5849a5e1
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-09 19:06:44 -0300

    chore(drizzle/sqlite): adds support for sqlite in drizzle config; working with dynamic database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,19 @@
+import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+
+const dbProvider = process.env.DATABASE_PROVIDER || 'pg';
 
 export default defineConfig({
   out: './src/db/drizzle/migrations',
-  schema: './src/db/drizzle/schemas.ts',
-  dialect: 'postgresql',
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
+  schema:
+    dbProvider === 'pg'
+      ? './src/db/drizzle/schemas/postgres-schemas.ts'
+      : './src/db/drizzle/schemas/sqlite-schemas.ts',
+  dialect: dbProvider === 'pg' ? 'postgresql' : 'sqlite',
+  dbCredentials:
+    dbProvider === 'pg'
+      ? {
+          url: process.env.DATABASE_URL!,
+        }
+      : { url: process.env.DATABASE_FILENAME_SQLITE || 'sqlite.db' },
 });
