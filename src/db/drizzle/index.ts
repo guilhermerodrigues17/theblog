9b26557397a4b1d163575e38a9b0961e5b325a8d
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-09 19:11:50 -0300

    feat(drizzle): adds support for sqlite and postgres; working with dynamic database

--- src/db/drizzle/index.ts
+++ src/db/drizzle/index.ts
@@ -1,14 +1,35 @@
 import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import * as schema from './schemas';
+import { drizzle as drizzlePostgres } from 'drizzle-orm/node-postgres';
+import { drizzle as drizzleSqlite } from 'drizzle-orm/better-sqlite3';
+import { postgresPostsTable } from './schemas/postgres-schemas';
 import { Pool } from 'pg';
+import Database from 'better-sqlite3';
+import { sqlitePostsTable } from './schemas/sqlite-schemas';
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL!,
+const dbProvider = process.env.DATABASE_PROVIDER || 'pg';
+const pgDbUrl = process.env.DATABASE_URL || '';
+
+const pgPool = new Pool({
+  connectionString: pgDbUrl,
   ssl:
     process.env.NODE_ENV === 'production'
       ? { rejectUnauthorized: false }
       : false,
 });
 
-export const db = drizzle(pool, { schema });
+const sqlite = new Database(
+  process.env.DATABASE_FILENAME_SQLITE || 'sqlite.db',
+);
+
+export const sqliteDb = drizzleSqlite({
+  client: sqlite,
+  schema: { postsTable: sqlitePostsTable },
+});
+
+export const postgresDb = drizzlePostgres(pgPool, {
+  schema: { postsTable: postgresPostsTable },
+});
+
+export function getDbInstance() {
+  return dbProvider === 'pg' ? postgresDb : sqliteDb;
+}
