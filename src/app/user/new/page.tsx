ef6b81df56975a48c68d9221ffbda7f867154c83
Guilherme Rodrigues <guilhermerodriguesm23@gmail.com>
2025-09-19 12:13:21 -0300

    feat(user): adds metadata to the user creation page

--- src/app/user/new/page.tsx
+++ src/app/user/new/page.tsx
@@ -1,4 +1,11 @@
 import { CreateUserForm } from '@/components/CreateUserForm';
+import { Metadata } from 'next';
+
+export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+  title: 'Crie sua conta',
+};
 
 export default function CreateUserPage() {
   return <CreateUserForm />;
